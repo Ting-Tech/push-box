8dcb6219b547ce346e0147725e5a79b3dc0354ce
AllenTing <67228710+Ting-Tech@users.noreply.github.com>
2024-02-24 14:05:55 +0800

    Add router

--- src/App.js
+++ src/App.js
@@ -1,162 +1,29 @@
-import './App.css';
-import { BoardList } from './BoardList.js';
-import { useState, useEffect } from 'react';
-import { GiBrickWall } from "react-icons/gi";
-import { FaDropbox } from "react-icons/fa";
-import { IoAccessibility } from "react-icons/io5";
-import { LuDot } from "react-icons/lu";
-import { FaFlag } from "react-icons/fa";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Game from './Game';
+import Home from './Home';
 
-function Game() {
-  const [Level, setLevel] = useState(0);
-  const [Board, setBoard] = useState(BoardList[Level].board);
-  const [PlayerPos, setPlayerPos] = useState(BoardList[Level].initPlayerPos);
-  let [x, y] = PlayerPos;
-
-  const handleKey = (event) => {
-    console.log(event.key);
-    let [newX, newY] = PlayerPos;
-    let [deltaX, deltaY] = [0, 0];
-
-    switch (event.key) {
-      case 'w':
-        newX--;
-        deltaX--;
-        break;
-      case 'a':
-        newY--;
-        deltaY--;
-        break;
-      case 's':
-        newX++;
-        deltaX++;
-        break;
-      case 'd':
-        newY++;
-        deltaY++;
-        break;
-      default:
-        break;
-    }
-
-    let newBoard = Board.slice(); // 先複製舊的板面
-
-    switch (newBoard[newX][newY]) {
-      case 2:
-        newBoard[newX][newY] = 1;
-        newBoard[x][y] = 2;
-        setBoard(newBoard);
-        setPlayerPos([newX, newY]);
-        break;
-      case 3:
-        let [nextX, nextY] = [0, 0];
-        nextX = newX + deltaX;
-        nextY = newY + deltaY;
-        if (newBoard[nextX][nextY] !== 0 & newBoard[nextX][nextY] !== 3) {
-          newBoard[newX][newY] = 1;
-          newBoard[x][y] = 2;
-          newBoard[nextX][nextY] = 3;
-          setBoard(newBoard);
-          setPlayerPos([newX, newY]);
-        }
-        break;
-      case 4:
-        newBoard[newX][newY] = 1;
-        newBoard[x][y] = 2;
-        setBoard(newBoard);
-        setPlayerPos([newX, newY]);
-        break;
-      default:
-        break;
-    }
-
-    BoardList[Level].dotPos.map((dot) => {
-      if (newBoard[dot[0]][dot[1]] !== 1 & newBoard[dot[0]][dot[1]] !== 3) {
-        newBoard[dot[0]][dot[1]] = 4;
-        setBoard(newBoard);
-      }
-    })
-  }
-
-  useEffect(() => {
-    // 添加事件監聽器
-    document.addEventListener('keydown', handleKey);
-    // 在組件卸載時移除監聽器
-    return () => {
-      document.removeEventListener('keydown', handleKey);
-    };
-  }, [x, y]); // 監聽x,y 是否有變化
-
-  const status = calStatus(Board, Level);
-  if (status === " Win!") {
-    let nextLevel = Level + 1;
-    if (nextLevel < BoardList.length) {
-      setLevel(nextLevel);
-      setPlayerPos(BoardList[nextLevel].initPlayerPos);
-      setBoard(BoardList[nextLevel].board);
-    }
-  }
-
-
-  return (
-    <>
-      <div>
-        {Board.map((row, rowIdx) => {
-          return (
-            <div key={`row-${rowIdx}`} className="boardRow">{
-              row.map((text, colIdx) => {
-                if (text === 0) {
-                  return <GiBrickWall key={`Gi-${colIdx}`} className='blockSize' />
-                }
-                else if (text === 1) {
-                  return <IoAccessibility key={`Io-${colIdx}`} className='blockSize' />
-                }
-                else if (text === 2) {
-                  return <LuDot key={`Lu-${colIdx}`} className='blockSize' />
-                }
-                else if (text === 3) {
-                  return <FaDropbox key={`Fa-${colIdx}`} className='blockSize' />
-                }
-                else if (text === 4) {
-                  return <FaFlag key={`Fa-${colIdx}`} className='blockSize' />
-                }
-              })}</div>)
-        })}
-      </div>
-      <div className="gameStatus">
-        <h2>Status:</h2>
-        <h2>{status}</h2>
-      </div>
-    </>
-  );
-}
-
-function calStatus(board, level) {
-  let allDots = BoardList[level].dotPos;
-  let count = 0;
-  for (let i = 0; i < allDots.length; i++) {
-    if (board[allDots[i][0]][allDots[i][1]] !== 3) {
-      count++;
-    }
-  }
-  if (count === 0) {
-    return " Win!";
-  }
-  else {
-    return ("remain" + count);
-  }
-}
-
-export default function App() {
-
-  return (
-    <>
-      <div className="gameBoard">
-        <h1>Push Box</h1>
-        <div className="game">
-          <Game />
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <h1>Push Box</h1>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav mr-auto">
+              <li><Link to={'/'} className="nav-link"> Home </Link></li>
+              <li><Link to={'/game'} className="nav-link"> Game </Link></li>
+            </ul>
+          </nav>
+          <hr />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/game' element={<Game />} />
+          </Routes>
         </div>
-      </div>
-    </>
-  );
+      </Router>
+    )
+  }
 }
+
+export default App;
---
+++ src/Game.js
@@ -0,0 +1,161 @@
+import './Game.css';
+import { BoardList } from './BoardList.js';
+import { useState, useEffect, Component } from 'react';
+import { GiBrickWall } from "react-icons/gi";
+import { FaDropbox } from "react-icons/fa";
+import { IoAccessibility } from "react-icons/io5";
+import { LuDot } from "react-icons/lu";
+import { FaFlag } from "react-icons/fa";
+
+function GameBoard() {
+    const [Level, setLevel] = useState(0);
+    const [Board, setBoard] = useState(BoardList[Level].board);
+    const [PlayerPos, setPlayerPos] = useState(BoardList[Level].initPlayerPos);
+    let [x, y] = PlayerPos;
+
+    const handleKey = (event) => {
+        console.log(event.key);
+        let [newX, newY] = PlayerPos;
+        let [deltaX, deltaY] = [0, 0];
+
+        switch (event.key) {
+            case 'w':
+                newX--;
+                deltaX--;
+                break;
+            case 'a':
+                newY--;
+                deltaY--;
+                break;
+            case 's':
+                newX++;
+                deltaX++;
+                break;
+            case 'd':
+                newY++;
+                deltaY++;
+                break;
+            default:
+                break;
+        }
+
+        let newBoard = Board.slice(); // 先複製舊的板面
+
+        switch (newBoard[newX][newY]) {
+            case 2:
+                newBoard[newX][newY] = 1;
+                newBoard[x][y] = 2;
+                setBoard(newBoard);
+                setPlayerPos([newX, newY]);
+                break;
+            case 3:
+                let [nextX, nextY] = [0, 0];
+                nextX = newX + deltaX;
+                nextY = newY + deltaY;
+                if (newBoard[nextX][nextY] !== 0 & newBoard[nextX][nextY] !== 3) {
+                    newBoard[newX][newY] = 1;
+                    newBoard[x][y] = 2;
+                    newBoard[nextX][nextY] = 3;
+                    setBoard(newBoard);
+                    setPlayerPos([newX, newY]);
+                }
+                break;
+            case 4:
+                newBoard[newX][newY] = 1;
+                newBoard[x][y] = 2;
+                setBoard(newBoard);
+                setPlayerPos([newX, newY]);
+                break;
+            default:
+                break;
+        }
+
+        BoardList[Level].dotPos.map((dot) => {
+            if (newBoard[dot[0]][dot[1]] !== 1 & newBoard[dot[0]][dot[1]] !== 3) {
+                newBoard[dot[0]][dot[1]] = 4;
+                setBoard(newBoard);
+            }
+        })
+    }
+
+    useEffect(() => {
+        // 添加事件監聽器
+        document.addEventListener('keydown', handleKey);
+        // 在組件卸載時移除監聽器
+        return () => {
+            document.removeEventListener('keydown', handleKey);
+        };
+    }, [x, y]); // 監聽x,y 是否有變化
+
+    const status = calStatus(Board, Level);
+    if (status === " Win!") {
+        let nextLevel = Level + 1;
+        if (nextLevel < BoardList.length) {
+            setLevel(nextLevel);
+            setPlayerPos(BoardList[nextLevel].initPlayerPos);
+            setBoard(BoardList[nextLevel].board);
+        }
+    }
+
+
+    return (
+        <>
+            <div>
+                {Board.map((row, rowIdx) => {
+                    return (
+                        <div key={`row-${rowIdx}`} className="boardRow">{
+                            row.map((text, colIdx) => {
+                                if (text === 0) {
+                                    return <GiBrickWall key={`Gi-${colIdx}`} className='blockSize' />
+                                }
+                                else if (text === 1) {
+                                    return <IoAccessibility key={`Io-${colIdx}`} className='blockSize' />
+                                }
+                                else if (text === 2) {
+                                    return <LuDot key={`Lu-${colIdx}`} className='blockSize' />
+                                }
+                                else if (text === 3) {
+                                    return <FaDropbox key={`Fa-${colIdx}`} className='blockSize' />
+                                }
+                                else if (text === 4) {
+                                    return <FaFlag key={`Fa-${colIdx}`} className='blockSize' />
+                                }
+                            })}</div>)
+                })}
+            </div>
+            <div className="gameStatus">
+                <h2>Status:</h2>
+                <h2>{status}</h2>
+            </div>
+        </>
+    );
+}
+
+function calStatus(board, level) {
+    let allDots = BoardList[level].dotPos;
+    let count = 0;
+    for (let i = 0; i < allDots.length; i++) {
+        if (board[allDots[i][0]][allDots[i][1]] !== 3) {
+            count++;
+        }
+    }
+    if (count === 0) {
+        return " Win!";
+    }
+    else {
+        return ("remain" + count);
+    }
+}
+
+class Game extends Component {
+    render() {
+        return (
+            <div className="game" >
+                <h1>Game</h1>
+                <GameBoard />
+            </div>
+        );
+    }
+}
+
+export default Game;
